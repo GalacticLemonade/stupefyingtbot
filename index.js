065eae0414b1cc355ae9aa85c04644c86bf0f14c
GalacticLemonade <aidenmoore1820@gmail.com>
2023-05-14 21:43:15 -0600

    update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const { token } = require('./config.json');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.on(Events.GuildMemberAdd, guildMemberAdd => {
+	console.log("Guild Member joined");
+  });
+
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.on('guildMemberAdd', member => {
-	console.log("member joined");
 });
 
 // Log in to Discord with your client's token
